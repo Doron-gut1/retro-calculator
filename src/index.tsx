78df61b5fb395c69cdd2abb8ee581cde9002a68e
Doron-gut1 <hodayyae@gmail.com>
2024-12-31 13:03:30 +0200

    fix: update index.tsx to use App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import RetroCalculator from './components/RetroCalculator';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <RetroCalculator />
+    <App />
   </React.StrictMode>
 );
