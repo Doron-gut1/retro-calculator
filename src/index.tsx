e69df0b1525c68a08af30e47d262d26f4015f904
Doron-gut1 <hodayyae@gmail.com>
2024-12-31 14:11:41 +0200

    fix: update index.tsx import statement

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RetroCalculator } from './components/RetroCalculator';
+import { RetroCalculator } from './components/RetroCalculator/RetroCalculator';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
