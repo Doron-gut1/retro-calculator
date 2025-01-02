6fb0a10bc27b1cf6bf2b361ca7ebb3ecbfcc348b
Doron-gut1 <hodayyae@gmail.com>
2025-01-02 16:39:50 +0200

    תיקון נתיב ייבוא RetroForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ErrorBoundary } from './components/ErrorBoundary';
-import { RetroForm } from './components/Form/RetroForm';
+import { RetroForm } from './components/RetroForm';
 
 const App: React.FC = () => {
   return (
