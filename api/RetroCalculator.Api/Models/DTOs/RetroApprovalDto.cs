b14f8b942fbcc92f44ef6b4eb9881a02b7cd1e51
Doron-gut1 <hodayyae@gmail.com>
2025-01-04 21:11:47 +0200

    feat: Add RetroApprovalDto

---
+++ api/RetroCalculator.Api/Models/DTOs/RetroApprovalDto.cs
@@ -0,0 +1,13 @@
+namespace RetroCalculator.Api.Models.DTOs;
+
+public class RetroApprovalDto
+{
+    public string PropertyId { get; set; }
+    public int JobNumber { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public bool IsHistorical { get; set; }
+    public bool IsDifferencesOnly { get; set; }
+    public string Notes { get; set; }
+    public List<RetroCalculationResultDto> Results { get; set; }
+}
