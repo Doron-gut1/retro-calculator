6207e29fa60b09eb4833148439f57b90d3b1b48d
Doron-gut1 <hodayyae@gmail.com>
2025-01-05 14:56:28 +0200

    feat: complete IRetroService interface

--- api/RetroCalculator.Api/Services/Interfaces/IRetroService.cs
+++ api/RetroCalculator.Api/Services/Interfaces/IRetroService.cs
@@ -1,10 +1,29 @@
+using RetroCalculator.Api.Models;
 using RetroCalculator.Api.Models.DTOs;
 
 namespace RetroCalculator.Api.Services.Interfaces;
 
 public interface IRetroService
 {
-    Task<List<RetroCalculationResultDto>> CalculateRetroAsync(RetroCalculationRequestDto calculation);
-    // TODO: Approval process will be implemented later
-    // Task<bool> ApproveRetroAsync(...);
+    /// <summary>
+    /// מבצע חישוב רטרו לנכס
+    /// </summary>
+    /// <param name="request">נתוני הבקשה (נכס, תאריכים, סוגי חיוב, גדלים ותעריפים)</param>
+    /// <param name="odbcName">שם חיבור ה-ODBC</param>
+    /// <returns>תוצאות החישוב</returns>
+    Task<IEnumerable<TempArnmforat>> CalculateRetroAsync(
+        RetroCalculationRequest request,
+        string odbcName);
+
+    /// <summary>
+    /// מחזיר את תוצאות החישוב לנכס ותהליך ספציפיים
+    /// </summary>
+    /// <param name="propertyId">מזהה הנכס</param>
+    /// <param name="jobNum">מזהה התהליך</param>
+    /// <param name="odbcName">שם חיבור ה-ODBC</param>
+    /// <returns>תוצאות החישוב</returns>
+    Task<IEnumerable<TempArnmforat>> GetRetroResultsAsync(
+        string propertyId,
+        int jobNum,
+        string odbcName);
 }
