30cf71c5085a21954c9654ab25cf7f406b84e6b9
Doron-gut1 <hodayyae@gmail.com>
2025-01-05 19:41:10 +0200

    Update IRetroService interface to use DataTable

--- api/RetroCalculator.Api/Services/Interfaces/IRetroService.cs
+++ api/RetroCalculator.Api/Services/Interfaces/IRetroService.cs
@@ -1,29 +1,10 @@
-using RetroCalculator.Api.Models;
+using System.Data;
 using RetroCalculator.Api.Models.DTOs;
 
 namespace RetroCalculator.Api.Services.Interfaces;
 
 public interface IRetroService
 {
-    /// <summary>
-    /// מבצע חישוב רטרו לנכס
-    /// </summary>
-    /// <param name="request">נתוני הבקשה (נכס, תאריכים, סוגי חיוב, גדלים ותעריפים)</param>
-    /// <param name="odbcName">שם חיבור ה-ODBC</param>
-    /// <returns>תוצאות החישוב</returns>
-    Task<IEnumerable<TempArnmforat>> CalculateRetroAsync(
-        RetroCalculationRequest request,
-        string odbcName);
-
-    /// <summary>
-    /// מחזיר את תוצאות החישוב לנכס ותהליך ספציפיים
-    /// </summary>
-    /// <param name="propertyId">מזהה הנכס</param>
-    /// <param name="jobNum">מזהה התהליך</param>
-    /// <param name="odbcName">שם חיבור ה-ODBC</param>
-    /// <returns>תוצאות החישוב</returns>
-    Task<IEnumerable<TempArnmforat>> GetRetroResultsAsync(
-        string propertyId,
-        int jobNum,
-        string odbcName);
+    Task<DataTable> CalculateRetroAsync(RetroCalculationRequestDto request, string odbcName);
+    Task<DataTable> GetRetroResultsAsync(string propertyId, int jobNum, string odbcName);
 }
