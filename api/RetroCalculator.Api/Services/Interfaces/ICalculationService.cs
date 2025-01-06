e3e4860b194683a9a613c81f4cbf1e537d5b074e
Doron-gut1 <hodayyae@gmail.com>
2025-01-06 10:17:52 +0200

    cleanup: remove duplicate services and unnecessary files

--- api/RetroCalculator.Api/Services/Interfaces/ICalculationService.cs
+++
@@ -1,8 +0,0 @@
-namespace RetroCalculator.Api.Services.Interfaces;
-
-public interface ICalculationService
-{
-    Task InitializeCalculationAsync(string propertyId, int jobNumber);
-    Task<bool> RunRetroCalculationAsync(int jobNumber, string odbcConnectionString);
-    Task<bool> ValidatePeriodAsync(DateTime startDate, DateTime endDate);
-}
