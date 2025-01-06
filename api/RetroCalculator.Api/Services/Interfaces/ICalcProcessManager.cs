37c33f7293141f718bc2ecaa78e1bdca60517b63
Doron-gut1 <hodayyae@gmail.com>
2025-01-06 10:37:12 +0200

    fix: add IDisposable to ICalcProcessManager

--- api/RetroCalculator.Api/Services/Interfaces/ICalcProcessManager.cs
+++ api/RetroCalculator.Api/Services/Interfaces/ICalcProcessManager.cs
@@ -1,16 +1,7 @@
 namespace RetroCalculator.Api.Services.Interfaces;
 
-public interface ICalcProcessManager
+public interface ICalcProcessManager : IDisposable
 {
-    /// <summary>
-    /// מפעיל את תהליך החישוב של הרטרו
-    /// </summary>
-    /// <param name="odbcName">שם חיבור ה-ODBC</param>
-    /// <param name="userName">שם המשתמש (לא בשימוש)</param>
-    /// <param name="jobNum">מזהה התהליך</param>
-    /// <param name="processType">סוג התהליך (תמיד 1)</param>
-    /// <param name="propertyId">מספר הנכס</param>
-    /// <returns>האם החישוב הצליח</returns>
     Task<bool> CalculateRetroAsync(
         string odbcName,
         string userName,
