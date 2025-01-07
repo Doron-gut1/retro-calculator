78ae2248c4b756ea399c8f092b4c2957b1074461
Doron-gut1 <hodayyae@gmail.com>
2025-01-07 08:55:06 +0200

    fix: simplify RetroCalculator component

--- src/components/RetroCalculator/RetroCalculator.tsx
+++ src/components/RetroCalculator/RetroCalculator.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { RetroForm } from '../RetroForm';
-import { PropertySearch } from '../Property/PropertySearch';
-import { PayerInfo } from '../Property/PayerInfo';
-import { SizesTable } from '../Property/SizesTable';
 
 export const RetroCalculator: React.FC = () => {
-  ...rest of the code...
+  return (
+    <div>
+      <RetroForm />
+    </div>
+  );
 };
