5e31bf46be231f10d66afa6daf7ee9e94356c9f4
Doron-gut1 <hodayyae@gmail.com>
2025-01-07 22:14:04 +0200

    fix: update ChargeTypesSelect export to named export

--- src/components/inputs/ChargeTypesSelect.tsx
+++ src/components/inputs/ChargeTypesSelect.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-interface ChargeTypesSelectProps {
+export interface ChargeTypesSelectProps {
   selected: string[];
   onChange: (types: string[]) => void;
 }
 
-const ChargeTypesSelect: React.FC<ChargeTypesSelectProps> = ({ selected, onChange }) => {
+export const ChargeTypesSelect: React.FC<ChargeTypesSelectProps> = ({ selected, onChange }) => {
   const handleChange = (type: string) => {
     if (selected.includes(type)) {
       onChange(selected.filter(t => t !== type));
@@ -36,5 +36,3 @@ const ChargeTypesSelect: React.FC<ChargeTypesSelectProps> = ({ selected, onChang
     </div>
   );
 };
-
-export default ChargeTypesSelect;
