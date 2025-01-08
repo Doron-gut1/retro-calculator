ed0f0e30294d92046ffc4129386203ce73f84fbf
Doron-gut1 <hodayyae@gmail.com>
2025-01-08 11:30:05 +0200

    fix: initialize session params on app load

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RetroForm } from './components/RetroForm';
+import { useRetroStore } from './store';
 
 const App: React.FC = () => {
+  const setSessionParams = useRetroStore(state => state.setSessionParams);
+
+  useEffect(() => {
+    // אתחול פרמטרים ראשוניים
+    setSessionParams({
+      odbcName: 'DefaultODBC',  // שם ה-ODBC שלך
+      jobNumber: 1  // מספר Job התחלתי
+    });
+  }, [setSessionParams]);
+
   return (
     <div dir="rtl">
       <RetroForm />
