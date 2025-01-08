e1e830c58659c10722be86404cad471880f08872
Doron-gut1 <hodayyae@gmail.com>
2025-01-08 15:15:47 +0200

    debug: add URL params logging to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { RetroForm } from './components/RetroForm';
 
 const App: React.FC = () => {
+  console.log('App rendering, URL params:', window.location.search);
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log('ODBC from URL:', urlParams.get('odbcName'));
+  console.log('JobNum from URL:', urlParams.get('jobNum'));
+
   return (
     <div dir="rtl">
       <RetroForm />
