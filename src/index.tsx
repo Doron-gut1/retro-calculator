b65c6273d4ca706f8237909283ff2c9341621628
Doron-gut1 <hodayyae@gmail.com>
2025-01-08 11:26:36 +0200

    fix: restore root rendering in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-//import { RetroCalculator } from './components/RetroCalculator.DEL';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-/* root.render(
+root.render(
   <React.StrictMode>
-    <RetroCalculator />
+    <App />
   </React.StrictMode>
-); */
+);
